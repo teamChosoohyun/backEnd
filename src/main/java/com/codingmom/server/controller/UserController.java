dbc85306a4ba818dcd3dac97d7c134de882d88e8
soohyuneeee <soohyuneeee12@gmail.com>
2022-11-19 12:34:56 +0900

    Feat - ShowUserInfo

--- src/main/java/com/codingmom/server/controller/UserController.java
+++ src/main/java/com/codingmom/server/controller/UserController.java
@@ -3,6 +3,7 @@ package com.codingmom.server.controller;
 import com.codingmom.server.domain.UserTbl;
 import com.codingmom.server.repository.UserRepository;
 import com.codingmom.server.service.KaKaoService;
+import org.springframework.data.repository.query.Param;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
@@ -45,6 +46,13 @@ public class UserController {
         userRepository.save(userTbl);
         return "success";
     }
+    @ResponseBody
+    @RequestMapping(value = "/user/join", method = RequestMethod.POST, produces = "application/json; charset=utf-8")
+    public UserTbl ShowUserInfo(@Param("id")Long user_id)throws Exception{
+        return userRepository.findById(user_id)
+                .orElseThrow();
+    }
+
 
 //    @ResponseBody
 //    @RequestMapping(value = "/user/update", method = RequestMethod.PUT, produces = "application/json; charset=utf-8")
