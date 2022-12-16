432aca9213ed3b3fd8912725749b47cc0f3b32d4
soohyuneeee <soohyuneeee12@gmail.com>
2022-12-16 12:34:56 +0900

    Add  - Swagger

--- src/main/java/com/codingmom/server/ServerApplication.java
+++ src/main/java/com/codingmom/server/ServerApplication.java
@@ -3,9 +3,18 @@ package com.codingmom.server;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 
 @SpringBootApplication(exclude = SecurityAutoConfiguration.class)
 public class ServerApplication {
+
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html")
+				.addResourceLocations("classpath:/META-INF/resources/");
+
+		registry.addResourceHandler("/webjars/**")
+				.addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(ServerApplication.class, args);
